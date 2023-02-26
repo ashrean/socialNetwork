0b9867ae2621c9f4900c0ae79a724a6ee1bb8d64
Ashley Sese <sese.ashrean@gmail.com>
2023-02-25 23:46:44 -0800

    adding routes

--- models/Thought.js
+++ models/Thought.js
@@ -4,6 +4,33 @@ const Reaction = require('./reaction');
 
 const Schema = mongoose.Schema;
 
+//Reaction Schema
+const reactionSchema = new Schema({
+    reactionID: {
+      type: Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // using the getter method on this field to format the timestamp using the `toLocaleString` when the document is retrieved from the database
+      get: function(value) {
+        return new Date(value).toLocaleString();
+      }
+    }
+});
+
+
+
 const ThoughtSchema = new Schema ({
     thoughtText: {
         type: String,
@@ -23,7 +50,7 @@ const ThoughtSchema = new Schema ({
         ref: 'User'
     },
 
-    reactions: [Reaction.schema]
+    reactions: [reactionSchema]
 });
 
 ThoughtSchema.virtual('reactionCount').get(function() {
@@ -33,4 +60,3 @@ ThoughtSchema.virtual('reactionCount').get(function() {
 const Thought = mongoose.model('Thought', ThoughtSchema);
 
 module.exports = Thought;
-
