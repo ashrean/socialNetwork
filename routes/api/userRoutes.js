c3ee56edf7166e6feb66edd3cc20f0f52a63e490
Ashley Sese <sese.ashrean@gmail.com>
2023-02-28 00:40:50 -0800

    testing phase

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,13 +5,13 @@ const {
     updateUser,
     deleteUser,
     getUserById,
+    addFriend,
     removeFriend
 } = require('../../controllers/userController')
 
 router.route('/').get(getAllUsers).post(createUser);
 
-router.route('/:id').get(getUsersById).put(updateUsers).delete(deleteUsers);
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
-router.route('/:id').get(getUsersById).put(updateUsers).delete(deleteUsers);
-
-module.exports = router; 
+router.route("/:id/friends/:friendId").post(addFriend).delete(removeFriend);
+module.exports = router;
--- utils/seed.js
+++ utils/seed.js
@@ -1,1 +1,26 @@
+const connection = require('../config/connection')
+const { Thought, User } = require('../models')
+const {faker} = require('@faker-js/faker')
 
+connection.on('error', (err) => console.log(err))
+
+connection.once('open', async () => {
+    console.log('connected to db');
+
+    await User.deleteMany({});
+
+    const users = [];
+
+    for (i = 0; i < 10; i++) {
+        users.push({
+            username: faker.internet,userName(),
+            email: faker.internet.email(),
+        });
+    }
+
+    await User.collection.insertMany(users);
+
+    console.table(users);
+    console.info('seeded ${users.length} users')
+    process.exit(0);
+});
